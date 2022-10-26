161b66087094ec0699af4b275bc02a4d377a5f6e
Karoliny Nascimento <108940073+KKojiio@users.noreply.github.com>
2022-10-26 08:20:49 -0300

    26/10

--- estudo.js
+++ estudo.js
@@ -1,55 +1,63 @@
-function moeda(atual){
-  return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+function moeda(atual) {
+  return atual.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 }
 
-function calcula(){
+function calcula() {
   let c = document.getElementById("valor").value;
   let j = document.getElementById("juros").value;
-  if (!Number(c)){
+  let t = document.getElementById("meses").value;
+
+  if (!Number(c)) {
     alert("O valor deve ser um Número");
     document.getElementById("valor").value = "";
     document.getElementById("valor").focus();
-   return
+    return
   }
-  if (!Number(j)){
+  if (!Number(j)) {
     alert("O valor deve ser um Número");
     document.getElementById("juros").value = "";
     document.getElementById("juros").focus();
-   return
+    return
   }
-  
-  let r = c * (1 + (j/100));
-  document.write("Resultado:" + moeda(r))
+  if (!Number(t)) {
+    alert("A quantidade de meses deve ser numérico");
+    document.getElementById("meses").value = "";
+    document.getElementById("meses").focus();
+    return
+
+    let r = c;
+    for (let m = 1; m <= t; m++) {
+      r = c * (1 + (j / 100));
+      c = r;
+      document.write("Mês" + " - valor: " + moeda(r) + "<br>")
+    }
+
+    document.write("Resultado:" + moeda(r))
+  }
 }
 
-let op = "";
-function operacao(ope){
-  op = ope;
-
-}
-function calcule (){
-  let v1 = document.getElementById("v1").value;
-  let v2 = document.getElementById("v2").value;
-  let r = 0;
- 
-  
-  if(op == "+"){
-     r = Number(v1) + Number(v2);
-   }
-   if(op == "-"){
-     r = Number(v1) - Number(v2);
-   }
-   if(op == "*"){
-     r = Number(v1) * Number(v2);
-   }
-  if(op == "/"){
-    r = Number(v1) / Number(v2);
-   }
- document.getElementById("resultado").innerHTML = r; 
- 
-}
+  let op = "";
+  function operacao(ope) {
+    op = ope;
+  }
+  function calcule() {
+    let v1 = document.getElementById("v1").value;
+    let v2 = document.getElementById("v2").value;
+    let r = 0;
 
 
+    if (op == "+") {
+      r = Number(v1) + Number(v2);
+    }
+    if (op == "-") {
+      r = Number(v1) - Number(v2);
+    }
+    if (op == "*") {
+      r = Number(v1) * Number(v2);
+    }
+    if (op == "/") {
+      r = Number(v1) / Number(v2);
+    }
 
-
-
+    document.getElementById("resultado").innerHTML = r;
+  }
